105b5ad73f5687ed6392ef69ff4c45f4d133ca75
MOHAMMADIzahra1401 <140957622+MOHAMMADIzahra1401@users.noreply.github.com>
2023-08-03 07:24:19 -0700

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -1,8 +1,10 @@
-zele1=int(input("Enter a number for zele1:"))
-zele2=int(input("Enter a number for zele2:"))
-zele3=int(input("Enter a number for zele3:"))
-
-if zele1< zele2+zele3 or  zele2< zele1+zele3  or zele3< zele1+zele2:
+zele1=float(input("Enter a number for zele1:"))
+zele2=float(input("Enter a number for zele2:"))
+zele3=float(input("Enter a number for zele3:"))
+if zele1>0 and zele2>0 and zele3>0:
+    if zele1< zele2+zele3 and  zele2< zele1+zele3  and zele3< zele1+zele2:
     print("it is a mosalas")
+    else:
+    print("it is not a mosalas")
 else:
     print("it is not a mosalas")
