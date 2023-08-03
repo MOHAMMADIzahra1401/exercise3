410fb7dd94b607936a962bcc34aee071845ce1e9
MOHAMMADIzahra1401 <140957622+MOHAMMADIzahra1401@users.noreply.github.com>
2023-08-03 07:15:42 -0700

    Add files via upload

---
+++ exercise3.py
@@ -0,0 +1,8 @@
+zele1=int(input("Enter a number for zele1:"))
+zele2=int(input("Enter a number for zele2:"))
+zele3=int(input("Enter a number for zele3:"))
+
+if zele1< zele2+zele3 or  zele2< zele1+zele3  or zele3< zele1+zele2:
+    print("it is a mosalas")
+else:
+    print("it is not a mosalas")
